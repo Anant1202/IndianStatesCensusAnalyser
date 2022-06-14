ae970a5e85fd75bbef140b60785dd55cc2303fd9
Anant1202 <anantsingh1202@gmail.com>
2022-06-14 16:01:40 +0530

    Welcome to Indian States Census Analyser Program

---
+++ IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser Program");
+        }
+    }
+}
+
